b027f14ca6f42cd29fb3531372c2a85931bdd671
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-09-10 20:07:46 +0600

    save model

--- server/server.js
+++ server/server.js
@@ -1,8 +1,33 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/meanproject');
+
 app.get('/', (req, res) => {
   res.send('Hello, world, 123');
+});
+
+const Post = mongoose.model('Post', {
+  title: String,
+  author: String,
+  content: String,
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+const newPost = new Post({
+  title: 'Tenge 381 v obmennikah',
+  author: 'Shamil Suvorov',
+  content: 'Segodnya bla bla blab bla...'
+});
+
+newPost.save().then(function(success) {
+  console.log('saved ', success);
+}, function(error) {
+  console.log('Error on save ', error);
 });
 
 app.listen(3000);
