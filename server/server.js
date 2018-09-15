6e8ef303bdfb3cb5fba2ec77a236848fb0f01cc8
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-09-15 20:00:53 +0600

    refactoring. CRUD with routing

--- server/server.js
+++ server/server.js
@@ -1,105 +1,121 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
+var mongoose = require('mongoose'); // getting mongoose library. Mongoose helps to work with MongoDB database
+var express = require('express'); // getting express library. Express helps add routing for our NodeJS application
+var bodyParser = require('body-parser'); // getting body-parser library. Body parser helps to barse body in requests
 
-mongoose.connect('mongodb://localhost:27017/meanproject');
+var app = express(); //call express function and save to 'app' variable
 
-app.get('/', (req, res) => {
-  res.send('Hello, world, 123');
+app.use(bodyParser.json()); // use bodyParser.json() function that helps to parse JSON in body requests
+
+mongoose.connect('mongodb://localhost:27017/meanproject'); //connecting to database with port 27017 and meanproject database
+
+app.get('/', function(req, res) { // GET / route. On 'localhost:3000' triggers function that return 'Trigger on url /' message
+  res.send('Trigger on url /');
 });
 
-const Post = mongoose.model('Post', {
+const Post = mongoose.model('Post', { // Creating our Post model(table).
   title: String,
   author: String,
   content: String,
   date: {
     type: Date,
-    default: Date.now()
+    default: Date.now() //setting default, which will take date time on saving
   }
 });
 
-const newPost = new Post({
-  title: 'Moy vtoroy post',
-  author: '123',
-  content: 'Segodnya bla bla blab bla...'
+/* SAVE NEW ITEM IN DATABSE */
+app.put('/posts', function(req, res) { // PUT /posts . Triggers function on PUT 'localhost:3000/posts' url.
+
+  var title = req.body.title; //getting 'title' property from body object
+  var content = req.body.content; //getting 'content' property from body object
+  var author = req.body.author; //getting 'author' property from body object
+
+  var post = new Post({
+    title: title,
+    content: content,
+    author: author
+  });
+
+
+  post.save(post)
+    .then( //handle success case
+      function(doc) { // doc stands for Document(row in table)
+        res.status(201).send(doc)} // return status '201' and saved doc(object)
+    ).catch( //handle error case
+      function(e) {
+        res.status(400) // return status 400. 400 usually means 'Bad input parameters'
+      }
+    );
+
 });
 
-/* SAVE POST */
-// newPost.save().then(function(success) {
-//   console.log('saved ', success);
-// }, function(error) {
-//   console.log('Error on save ', error);
-// });
-
-
-/* GET ALL */
-// Post.find().then(function(success) {
-//   console.log('success ', success);
-// }, function(error) {
-//   console.log('error happened on find ', error);
-// });
+/* GET ALL POSTS */
+app.get('/posts', function(req, res) { // GET '/posts' url. Triggers function on GET 'localhost:3000/posts' url
+  Post.find().then( //finds all items in table 'post'
+    function(posts) { // success case function
+      res.send({posts: posts}) //return object , where with all posts from table
+    }
+  ).catch(
+    function(e) { // handle error case. e - error
+      res.status(400).send(e); //return status 400 and our error
+    }
+  );
+});
 
 /* GET POST BY ID */
-// Post.findById('5b967dc4ac7425190421ed7c')
-//   .then(function(success) {
-//     console.log('findById ', success);
-//   }, function(error) {
-//     console.log('error happend on findById ', error);
-//   });
+app.get('/posts/:id', function(req, res) { // GET '/posts/:id' url. Triggers function on GET 'localhost:3000/posts/someIdValue' url
+  var id = req.params.id; //saving our someIdValue to id variable
 
-/* UPDATE BY ID. findByIdAndUpdate */
-/* DELETE BY ID. findByIdAndDelete */
-
-app.get('/api/posts', function(req, res) {
-  Post.find().then(function(success) {
-    res.send({posts: success});
-  }, function(error) {
-    console.log('error happened on find ', error);
-  });
+  Post.findById(id).then(
+    function(post) { //handle success case. where 'post' is response value
+      res.send({post: post}); // return found post object
+    }
+  ).catch(
+      function(e) { // handle error case
+        res.status(400).send(e); //return status 400 and our error as response. 400 means 'Bad input'
+      }
+  );
 });
 
-app.get('/api/posts/:id', function(req, res) {
-  var id = req.params.id;
+/* DELETE POST BY ID */
+app.delete('/posts/:id', function(req, res) { // DELETE '/posts/:id' url. Triggers function on DELETE 'localhost:3000/posts/someIdValue' url
+  var id = req.params.id; //saving our someIdValue to id variable
 
-  Post.findById(id)
-    .then(function(success) {
-      console.log('success ', success);
-      res.send(success);
-    }, function(error) {
-      console.log('error happend on findById ', error);
-  });
+  Post.findByIdAndRemove(id)
+    .then(
+      function(doc) { //handle success case. Where 'doc' is our response value from calling 'findByIdAndRemove' function
+        res.send(doc); // send deleted post as a response
+      }
+    )
+    .catch(
+      function(err) {  //handle fail case. Where 'err' is fail response message
+        res.status(404).send() // send status 400 which means 'Not Found' and send nothing as response
+      }
+    );
 });
 
-// localhost:3000/api/posts/asdfasfaasd
-app.delete('/api/posts/:myid', function(req, res) {
-  var id = req.params.myid;
+app.post('/posts/:id', function(req, res) { // POST '/posts/:id' . Trigger function on POST 'localhost:3000/posts/someIdValue' url
+  var id = req.params.id; //saving our someIdValue to id variable
 
-  Post.findByIdAndDelete(id)
-    .then(function(success) {
-      console.log('success ', success);
-    }, function(error) {
-      console.log(error);
-    });
-});
+  var title = req.body.title; //getting 'title' property from body object
+  var content = req.body.content; //getting 'content' property from body object
+  var author = req.body.author; //getting 'author' property from body object
 
-app.post('/api/posts/:id', function(req, res) {
-  var author = req.body.author;
-  var content = req.body.content;
-  var id = req.params.id;
-
-  var updateObject = {
+  var data = {
+    title: title,
     author: author,
     content: content
   };
 
-  Post.findByIdAndUpdate(id, {$set: updateObject} ,
-    {new: true})
-    .then(function(success) {
-      console.log('udated ', success);
-    }, function(error) {
-      console.log('error ', error);
-    });
+  Post.findByIdAndUpdate(id, {$set: data}, {new: true}) // {$set: data} means to replace existing object in database to this new 'data' object
+    .then(                                              // {new: true} means to return new updated object
+      function(doc) { //handle success case , where 'doc' parameter means our new updated object(document)
+        res.send(doc); //send updated document object as a response
+      }
+    ).catch(
+      function(err) { //handle error case, where 'err' parameter is our err response message
+        res.status(400).send(err); //send status 400 and err message as a response. 400 status means 'Bad input'
+      }
+    );
 });
-
 
 app.listen(3000);
