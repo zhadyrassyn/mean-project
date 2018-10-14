e99e4598db830d02ae8fe118bd3dc09c3d3a9b22
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-10-14 15:35:26 +0600

    finish authorization

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ passport.use(new LocalStrategy(
         return done(null, err, {message: 'Incorrect email'});
       }
 
-      user.comparePassword(passport, function(err, isMatch) {
+      user.comparePassword(password, function(err, isMatch) {
         if (err) {
           return done(err);
         }
@@ -47,10 +47,12 @@ passport.use(new LocalStrategy(
 ));
 
 passport.serializeUser(function(user, done) {
+  console.log('serialize ', user);
   done(null, user._id);
 });
 
 passport.deserializeUser(function(id, done) {
+  console.log('deserialize ', id);
   User.findById(id, function(err, user) {
     done(err, user);
   });
@@ -61,6 +63,48 @@ app.use(passport.session());
 
 app.use(bodyParser.json()); // use bodyParser.json() function that helps to parse JSON in body requests
 
+
+
+app.post('/api/login', passport.authenticate('local'), function(req, res) {
+  res.sendStatus(200);
+});
+
+app.get('/shiro', function(req, res) {
+  res.cookie("BEKA", "ZNACHENIE BEKI");
+  if (req.isAuthenticated()) {
+    res.send("SHIRO IS HERE");
+  } else {
+    res.send("USER IS UNAUTHORIZED");
+  }
+});
+
+app.post('/api/signup', function(req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+
+  const user = new User({
+    email,
+    password,
+    firstName,
+    lastName,
+  });
+
+  user.save().then(function(user) {
+    req.login(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+    });
+
+    res.send({user});
+  }).catch(function(error) {
+    console.log('error ', error);
+    res.send(error);
+  });
+
+});
 
 app.use('/api', postController);
 
